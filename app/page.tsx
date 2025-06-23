a229fb5fe64aa4178e0f9a656500fcbc73baa2a5
siddharth0248 <sc0248@uah.edu>
2025-06-23 11:49:07 -0500

    flood updates

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const mainTopSectionData = [
   },
   {
     title: 'Floods',
-    description:'Monitoring floods globally with advanced models and remote sensing data.',
+    description:'Data-driven insights for flood preparedness.',
     img: '/images/homepage/flood_theme.png',
     path: '/themes/floods',
   },    
@@ -39,27 +39,12 @@ export default function HomePage() {
       <div className='grid-container'>
         <div className='grid-row margin-top-5'>
           <h2>
-          Introduction text for Water Insight (draft text for Earth.gov Water Resources page) 
+          Welcome
           </h2>
           <div className='body-text'>
-            <p>
-              Water is arguably the most important resource for sustaining ecosystems, agriculture, and life itself. The water cycle describes how water moves throughout the planet’s atmosphere, oceans, and land impacting weather patterns, water availability and natural disasters such as floods and droughts. Although water covers the majority of the Earth’s surface, only 1% of it is readily available for our uses.
-            </p>
-            <p>
-              Using a combination of Earth observing satellites and sensors, ground-based monitoring and scientific modeling, we can observe Earth’s water resources at all stages of the water cycle. An accurate understanding of the terrestrial water cycle is important for developing resilience across multiple sectors.
-            </p>
-            <p>
-              Clean water is a fundamental human need, and understanding water cycle patterns help ensure sustainable management of freshwater resources for drinking, sanitation, commerce, and agriculture. Tracking precipitation, soil moisture, and groundwater levels helps predict floods and droughts, allowing communities to prepare and minimize damage.
-            </p>
-            <p>
-              Accurate data on soil moisture, precipitation, and groundwater levels can help farmers optimize irrigation, reduce water waste and increase crop yields. Understanding water availability is critical in the energy sector for efficient power generation and avoiding resource shortages. The insurance sector benefits by evaluating flood and drought risks and improving disaster modeling.
-            </p>
-            <p>
-              Water management efforts require satellite information to develop sustainable solutions for water conservation and distribution, while infrastructure development requires assessment of water-related risks. Thus, monitoring of the water cycle enables us to make data-driven decisions, reduce costs, and ultimately promote societal well-being and stability.
-            </p>
-            <p>
-              Federal agencies are working together to develop a terrestrial water cycle monitoring and forecasting system for North America to enable accurate assessment of critical variables such as precipitation, soil moisture, snow, groundwater, and streamflow for water resources management.
-            </p>
+          <p>
+          NASA Water Insight products provide essential information for managing water resources for the benefit of all sectors of society.
+          </p>
           </div>
         </div>
         <div className='grid-row grid-gap-lg card--homepage-main margin-top-2'>
