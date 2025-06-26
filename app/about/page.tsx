c39eaa628d64864a7a8e192f4f737338f35d3a19
siddharth0248 <sc0248@uah.edu>
2025-06-26 16:20:16 -0500

    remove href from page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,10 @@
+import React from 'react';
+
 export default function Page() {
   return (
     <div className='grid-container'>
       <p>
-        NASA Water Insight combines NASA&apos;s unique and trusted satellite-based Earth observations with state-of-the-art land, weather, and water modeling capabilities to produce information for water resource managers, farmers, weather forecasters, private industry, and the public. Working with community members, data is transformed to useful information products available via this open-access dashboard and our partner agencies. Learn more about our products by exploring Data Stories and the Data Catalog on this dashboard and on our website here:{' '}
-        <a
-          href='https://ldas.gsfc.nasa.gov/nldas/v3'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          https://ldas.gsfc.nasa.gov/nldas/v3
-        </a>.
+      NASA Water Insight combines NASA’s unique and trusted satellite-based Earth observations with state-of-the-art land, weather, and water modeling capabilities to produce information for water resource managers, farmers, weather forecasters, private industry, and the public. Working with community members, data is transformed to useful information products available via this open-access dashboard and our partner agencies. Learn more about our products by exploring Data Stories and the Data Catalog on this dashboard and on our website here: https://ldas.gsfc.nasa.gov/nldas/v3.
       </p>
     </div>
   );
