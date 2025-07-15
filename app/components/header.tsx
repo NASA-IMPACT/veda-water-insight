6c5b19c2a77d6a7146d203ec88ba03ac3c8be6f0
Kyle Lesinger <kdl0040@uah.edu>
2025-07-15 14:00:09 -0500

    update contact page

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -50,8 +50,8 @@ export const subNavItems: NavItem[] = [
   {
     id: 'contact-us',
     title: 'Contact us',
-    //actionId: 'open-google-form',
-    //type: 'action',
+    to: '/contact',
+    type: 'internalLink',
   },
 ];
 
